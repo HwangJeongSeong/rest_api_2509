17e932f5a7eb5128cbb7efe0a8e87a4d0a721f6d
HwangJeongSeong <hjs54336297@gmail.com>
2025-09-22 04:12:36 +0900

    RsData

--- src/main/java/com/example/demo/global/jpa/BaseEntity.java
+++ src/main/java/com/example/demo/global/jpa/BaseEntity.java
@@ -1,7 +1,11 @@
 package com.example.demo.global.jpa;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -9,7 +13,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
+@SuperBuilder
 @MappedSuperclass
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
@@ -20,7 +24,7 @@ public class BaseEntity {
     private Long id;
 
     @CreatedDate
-    private LocalDateTime createDate;
+    private LocalDateTime createdDate;
 
     @LastModifiedDate
     private LocalDateTime modifiedDate;
