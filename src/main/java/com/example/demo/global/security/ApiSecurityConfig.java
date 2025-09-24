270422f0c5f124841c53cce1870227146fffe30d
황정성 <hwangjeongseong@sexyguyhwangjeongseong-ui-MacBookAir.local>
2025-09-24 16:56:46 +0900

    login

--- src/main/java/com/example/demo/global/security/ApiSecurityConfig.java
+++ src/main/java/com/example/demo/global/security/ApiSecurityConfig.java
@@ -1,29 +1,44 @@
 package com.example.demo.global.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+@RequiredArgsConstructor
+public class ApiSecurityConfig {
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain apifilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+                .securityMatcher("/api/**")
+                .authorizeRequests(
+                        authorizeRequests -> authorizeRequests
+                                .requestMatchers(HttpMethod.GET, "/api/*/articles").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/*/articles/*").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/*/members/login").permitAll() // 로그인은 누구나 가능, post 요청만 허용
+                                .requestMatchers(HttpMethod.GET, "/api/*/members/me").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .csrf(
+                        csrf -> csrf
+                                .disable()
+                ) // csrf 토큰 끄기
+                .httpBasic(
+                        httpBasic -> httpBasic.disable()
+                ) // httpBasic 로그인 방식 끄기
+                .formLogin(
+                        formLogin -> formLogin.disable()
+                ) // 폼 로그인 방식 끄기
+                .sessionManagement(
+                        sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
         ;
         return http.build();
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
