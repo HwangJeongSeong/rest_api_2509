0e7f174b50c62e143ebda7d4d93fe9545e5199b8
황정성 <hwangjeongseong@sexyguyhwangjeongseong-ui-MacBookAir.local>
2025-09-24 15:16:34 +0900

    jwt

--- src/main/java/com/example/demo/domain/member/controller/ApiV1MemberController.java
+++ src/main/java/com/example/demo/domain/member/controller/ApiV1MemberController.java
@@ -1,17 +1,31 @@
 package com.example.demo.domain.member.controller;
 
+import com.example.demo.domain.member.dto.request.MemberRequest;
+import com.example.demo.domain.member.dto.response.MemberResponse;
+import com.example.demo.domain.member.entity.Member;
 import com.example.demo.domain.member.service.MemberService;
+import com.example.demo.global.RsData.RsData;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
-@RequestMapping(value = "/api/v1/memvers", produces = APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/api/v1/members", produces = APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
-@Tag(name = "ApiV1ArticleController", description = "회원 인증/인가 API")
+@Tag(name = "ApiV1MemberController", description = "회원 인증/인가 API")
 public class ApiV1MemberController {
     private final MemberService memberService;
+    
+    @PostMapping("/join")
+    public RsData<MemberResponse> join (@Valid @RequestBody MemberRequest memberRequest) {
+        Member member = memberService.join(memberRequest.getUsername(), memberRequest.getPassword());
+        
+        return RsData.of("200", "회원가입이 완료되었습니다.", new MemberResponse(member));
+    }
 }
